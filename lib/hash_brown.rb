fc4acb034b2f0fd8d6880227d8e5f6f5ce04ab21
Ryland Herrick <ryland@bendyworks.com>
2012-01-05 21:44:43 -0600

    Refactor
    Use implicit self, shorter alias for has_key?

--- lib/hash_brown.rb
+++ lib/hash_brown.rb
@@ -2,8 +2,8 @@ require 'hash_brown/version'
 
 class Hash
   def method_missing meth, *args, &block
-    return self[meth] if self.has_key? meth
-    return self[meth.to_s] if self.has_key? meth.to_s
+    return self[meth] if key? meth
+    return self[meth.to_s] if key? meth.to_s
     super
   end
 end
